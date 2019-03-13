e9e686b49cf200947cc0b990587f1ace3887d22a
Debora <me.imagine.debby@gmail.com>
2019-03-13 10:05:28 -0400

    uploading the files

---
+++ sources/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {             
+$('.js--section').waypoint(function(direction) {
+    if(direction == "down"){
+        $('nav').addClass('sticky');
+    } else{
+        $('nav').removeClass('sticky');
+    }
+},{
+    offset:'60px;'
+});
+
+
+/*var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})*/
+                  
+
+ });
