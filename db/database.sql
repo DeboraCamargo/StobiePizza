4ea12f9013bc4a003cb8c39b1b24ae1e3bc0702a
Debora <me.imagine.debby@gmail.com>
2019-03-22 17:37:06 -0400

    database

--- db/database.sql
+++ db/database.sql
@@ -12,52 +12,134 @@ GRANT all privileges ON pizzadb.* TO 'group3user'@'::1' IDENTIFIED BY 'Test123!'
 
 USE pizzadb;
 
-DROP TABLE IF EXISTS `user`;
-DROP TABLE IF EXISTS `account`;
-DROP TABLE IF EXISTS `pizza`;
+DROP TABLE IF EXISTS `address`;
+DROP TABLE IF EXISTS `Profile`;
+DROP TABLE IF EXISTS `userLogin`;
+DROP TABLE IF EXISTS `sauce`;
 DROP TABLE IF EXISTS `topping`;
-DROP TABLE IF EXISTS `Order`;
+DROP TABLE IF EXISTS `pizzaSize`;
+DROP TABLE IF EXISTS `customPizza`;
+DROP TABLE IF EXISTS `preDefinedPizza`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `orderDetails`;
 
-create table `account`
+
+CREATE TABLE `address`
 (
-`account_id` int(4) not null auto_increment,
-`first_name` varchar(50) not null,
-`last_name` varchar(50) not null,
-`email` varchar(50) not null,
-`password` varchar(50) not null,
-`user_name` varchar(50) not null,
-primary key(`account_id`)
+ `addressId`  integer NOT NULL auto_increment,
+ `streetName` varchar(100) NOT NULL ,
+ `numberAdd`  varchar(6) NOT NULL ,
+ `postalcode` varchar(6) NOT NULL ,
+ `city`       varchar(100) NOT NULL ,
+ `province`   varchar(100) NOT NULL ,
+PRIMARY KEY (`addressId`)
 );
 
-create table `user`
+CREATE TABLE `Profile`
 (
-`user_id` int(4) not null auto_increment,
-`loginName` varchar(20) not null,
-`password` varchar(6) not null,
-`rememberMe` bit,
-`account_id` int(4) not null,
- `email_user` varchar(20) not null,
- primary key(`user_id`),
- 	FOREIGN KEY(`account_id`) REFERENCES account(`account_id`)
+ `profile_id`   integer NOT NULL auto_increment ,
+  `addressId`   integer NOT NULL ,
+ `email`        varchar(100) NOT NULL ,
+ `firstName`    varchar(100) NOT NULL ,
+ `lastName`     varchar(100) NOT NULL ,
+ `phonecontact` varchar(11) NOT NULL ,
+PRIMARY KEY (`profile_id`),
+FOREIGN KEY (`addressId`) REFERENCES `address` (`addressId`)
 );
 
-
-create table `pizza`(
-`pizza_id` int(4) not null auto_increment,
-  primary key(`pizza_id`)
-);
-
-create table `topping`
+CREATE TABLE `userLogin`
 (
-`topping_id` int(4) not null auto_increment,
-`name` varchar(200) not null,
-primary key(`topping_id`)
-
+ `userId`     integer NOT NULL auto_increment,
+  `profile_id` integer NOT NULL,
+ `login`      varchar(20) NOT NULL unique,
+ `password`   varchar(20) NOT NULL,
+ `rememberme` bit NOT NULL ,
+PRIMARY KEY (`userId`),
+ FOREIGN KEY (`profile_id`) REFERENCES Profile(`profile_id`)
 );
 
-create table `order`
+CREATE TABLE `sauce`
 (
-`order_id` int(4) not null auto_increment,
-`orderName` varchar(100) not null,
-  primary key(`order_id`)
+ `sauceId` integer NOT NULL auto_increment ,
+ `name`    varchar(45) NOT NULL ,
+ `price` decimal not null,
+PRIMARY KEY (`sauceId`)
 );
+
+CREATE TABLE `topping`
+(
+ `toppingId` integer NOT NULL auto_increment ,
+ `price` decimal not null,
+ `name`      varchar(45) NOT NULL ,
+ `side`      varchar(45) NOT NULL ,
+PRIMARY KEY (`toppingId`)
+);
+
+create table `pizzaSize`
+(
+`pizzaSizeId` integer NOT NULL auto_increment,
+ `name`    varchar(45) NOT NULL ,
+ `price` decimal not null,
+PRIMARY KEY (`pizzaSizeId`)
+);
+
+CREATE TABLE `customPizza`
+(
+ `customPizzaId` integer NOT NULL auto_increment,
+ `toppingId`   integer NOT NULL ,
+ `sauceId`     integer NOT NULL ,
+ `pizzaSizeId`   integer  NOT NULL ,
+ `Crust`   varchar(30)  NOT NULL ,
+PRIMARY KEY (`customPizzaId`),
+FOREIGN KEY (`toppingId`) REFERENCES `topping` (`toppingId`),
+FOREIGN KEY (`sauceId`) REFERENCES `sauce` (`sauceId`),
+FOREIGN KEY (`pizzaSizeId`) REFERENCES `pizzaSize` (`pizzaSizeId`)
+);
+
+CREATE TABLE `preDefinedPizza`
+(
+ `preDefinedPizzaId` integer NOT NULL auto_increment,
+  `customPizzaId` integer NOT NULL ,
+  `name` varchar(30) NOT NULL ,
+PRIMARY KEY (`preDefinedPizzaId`),
+FOREIGN KEY (`customPizzaId`) REFERENCES `customPizza` (`customPizzaId`)
+);
+
+CREATE TABLE `orders`
+(
+ `ordersId` integer NOT NULL auto_increment,
+ `customPizzaId` integer,
+  `preDefinedPizzaId` integer,
+ `price` decimal NOT NULL ,
+ `specialInstructions` varchar(200),
+PRIMARY KEY (`ordersId`),
+FOREIGN KEY (`customPizzaId`) REFERENCES `customPizza` (`customPizzaId`),
+FOREIGN KEY (`preDefinedPizzaId`) REFERENCES `preDefinedPizza` (`preDefinedPizzaId`)
+
+);
+
+create table `payment`
+(
+`paymentId` integer not null auto_increment,
+`methodName` varchar(30) not null,
+PRIMARY KEY (`paymentId`)
+);
+
+create table `orderDetails`
+(
+ `orderDetailsId` integer NOT NULL auto_increment,
+ `ordersId` integer NOT NULL ,
+ `profile_id`    integer NOT NULL ,
+ `paymentId` integer NOT NULL ,
+ `deliveryMethod` varchar(45) NOT NULL ,
+ `orderTiming` datetime,
+  `total`  decimal NOT NULL ,
+   PRIMARY KEY (`orderDetailsId`),
+   FOREIGN KEY (`profile_id`) REFERENCES `Profile` (`profile_id`),
+   FOREIGN KEY (`ordersId`) REFERENCES `orders` (`ordersId`),
+   FOREIGN KEY (`paymentId`) REFERENCES `payment` (`paymentId`)
+);
+
+
+
+
