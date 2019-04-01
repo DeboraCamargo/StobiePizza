d4eecc2cf14bb85118dd994e5eddbd88fb0244fb
Debora <me.imagine.debby@gmail.com>
2019-04-01 18:42:51 -0400

    idk

--- Project/index.php
+++ Project/index.php
@@ -45,7 +45,7 @@ $_SESSION['currentPage'] = 'start';
             <img src="./img/logo.png" alt="Stobie’s Pizza logo" class="logo">
             <img src="./img/logo.png" alt="Stobie’s Pizza logo" class="logo-black">
             <ul class="main-nav"> 
-                <li><a href="cart.php"><ion-icon name="cart"></ion-icon> (<?php if(count($_SESSION["cart"]){//echo "0";}else echo (count($_SESSION["cart"])); ?>)</a></li>
+                <li><a href="cart.php"><ion-icon name="cart"></ion-icon> (<?php echo (count($_SESSION["cart"]));?>)</a></li>
                 <li><a href="#">Sign up</a></li>
                 <li><a href="custompizza.php">Pizza Builder</a></li>
                 <li><a href="#">Specialties</a></li>
